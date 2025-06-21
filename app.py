5e74c6929112c8dd9aebebc979101a4d669bcea3
Barnabé Frimaudeau <frimaudeau.barnabe@gmail.com>
2025-06-21 16:17:56 -0400

    Add tasks to a list with the button

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -7,6 +7,16 @@ task_id = 0
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", tasks=tasks)
 
-app.run(debug=True)
+@app.route("/add", methods=['POST'])
+def add_task():
+    global task_id
+    task_name = request.form.get("task_name")
+    if task_name:
+        tasks.append({'id': task_id, 'name': task_name, 'completed': False})
+        task_id += 1
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
